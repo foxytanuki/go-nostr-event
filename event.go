25d588bda54d8c6d089cf0e4c2bb07a5fc449eff
foxytanuki <biz@foxytanuki.dev>
2023-03-04 15:56:06 +0900

    update type name CustomEvent to WrappedEvent

--- event.go
+++ event.go
@@ -6,33 +6,33 @@ import (
 	"github.com/nbd-wtf/go-nostr"
 )
 
-type CustomEvent struct {
+type WrappedEvent struct {
 	nostr.Event
 }
 
-func (cev *CustomEvent) SignPk(sk string) error {
+func (wev *WrappedEvent) SignPk(sk string) error {
 	pub, err := nostr.GetPublicKey(sk)
 	if err != nil {
 		return err
 	}
-	cev.PubKey = pub
-	if err := cev.Sign(sk); err != nil {
+	wev.PubKey = pub
+	if err := wev.Sign(sk); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func NewEvent(kind int, tags nostr.Tags, content string) CustomEvent {
+func NewEvent(kind int, tags nostr.Tags, content string) WrappedEvent {
 	ev := nostr.Event{
 		Kind:      kind,
 		CreatedAt: time.Now(),
 		Tags:      tags,
 		Content:   content,
 	}
-	cev := CustomEvent{
+	wev := WrappedEvent{
 		Event: ev,
 	}
 
-	return cev
+	return wev
 }
